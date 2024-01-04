d07a07cfb23446b8e279e1bf58fb51e62e31db55
Foreverhighness <Foreverhighness@github.com>
2024-01-04 13:07:14 +0800

    ✨ (ch5) using semaphore to sync.

--- ostep-homework/code/ch5/q3.c
+++ ostep-homework/code/ch5/q3.c
@@ -4,11 +4,11 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/sem.h>
 #include <unistd.h>
 
 /* man 2 pipe */
-
-int main() {
+void pipe_ipc() {
   int pipe_fd[2];
   assert(pipe(pipe_fd) == 0);
   int read_fd = pipe_fd[0];
@@ -40,6 +40,40 @@ int main() {
 
     printf("goodbye\n");
     close(read_fd);
+    exit(EXIT_SUCCESS);
   }
+}
+
+/* man 2 semop */
+void sem_ipc() {
+  const int semid = semget(IPC_PRIVATE, 1, 0600);
+  // set sem.val to 1
+  assert(semctl(semid, 0, SETVAL, 1) == 0);
+
+  const int rc = fork();
+  assert(rc >= 0);
+  if (rc == 0) {
+    // child
+    printf("hello\n");
+
+    // decrease sem.val (1 -> 0)
+    struct sembuf sop = {.sem_num = 0, .sem_op = -1, .sem_flg = 0};
+    assert(semop(semid, &sop, 1) == 0);
+
+    _exit(EXIT_SUCCESS);
+  } else {
+    // parent
+    // wait for sem.val become 0
+    struct sembuf sop = {.sem_num = 0, .sem_op = 0, .sem_flg = 0};
+    assert(semop(semid, &sop, 1) == 0);
+    printf("goodbye\n");
+
+    semctl(semid, 0, IPC_RMID);
+    exit(EXIT_SUCCESS);
+  }
+}
+
+int main() {
+  sem_ipc();
   return 0;
 }
