077e3c806b10d86fa23ef5a7c88bfac8064a52f1
Foreverhighness <Foreverhighness@github.com>
2024-01-12 23:09:27 +0800

    🔨 (ch13) update ch13 script.

--- ostep-homework/code/ch13/ch13.sh
+++ ostep-homework/code/ch13/ch13.sh
@@ -1,58 +1,65 @@
 #!/bin/bash
 
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+function echo_red() {
+  echo -e "${RED}${1}${NC}"
+}
+function echo_green() {
+  echo -e "${GREEN}${1}${NC}"
+}
 
-make build/memory-user &>/dev/null
+make all &>/dev/null
 
-echo Q1:
+echo_red Q1:
 man 1 free
 
-echo Q2:
+echo_red Q2:
 free -m
 free -h
 
-echo Q4:
+echo_red Q4:
 
-echo -e "${GREEN}Before:${NC}"
+echo_greed "Before:"
 free -m
 build/memory-user 1000 5 &
 pid=$!
 
 sleep 2s
-echo -e "${GREEN}After:${NC}"
+echo_greed "After:"
 free -m
 
 kill -9 $pid
 wait $pid
 
-echo -e "${GREEN}Killed:${NC}"
+echo_greed "Killed:"
 free -m
 
 wait # clear
-echo Q5:
+echo_red Q5:
 man 1 pmap
 
-echo Q6:
+echo_red Q6:
 build/memory-user 1000 3 &
 pid=$!
 sleep 2s
 
-echo -e "${GREEN}Without -x flag:${NC}"
+echo_greed "Without -x flag:"
 pmap $pid
 
-echo -e "${GREEN}With -x flag:${NC}"
+echo_greed "With -x flag:"
 pmap -x $pid
 
 wait # clear
-echo Q7:
+echo_red Q7:
 build/memory-user 1000 3 &
 pid=$!
 sleep 2s
 pmap -X $pid
 
 wait # clear
-echo Q8:
+echo_red Q8:
 for memory in {1,10,100,256}; do
   build/memory-user "$memory" 2 &
   pid=$!
