2a90461d3dbae5dc46b82b06f24c46ea85d926ff
Foreverhighness <Foreverhighness@github.com>
2024-01-13 00:55:06 +0800

    ✨ (lottery) add q2 probability calculator.

--- ostep-homework/cpu-sched-lottery/ch9.sh
+++ ostep-homework/cpu-sched-lottery/ch9.sh
@@ -8,6 +8,24 @@ done
 
 echo Q2:
 ./lottery.py -l "10:1,10:100" -c | grep "DONE"
+python3 <<EOF
+from functools import lru_cache
+from fractions import Fraction
+
+def calc(m, n, p):
+  @lru_cache(None)
+  def dp(x, y):
+    if x == -1 or y == -1: return Fraction(0)
+    if (x, y) == (0, 0): return Fraction(1)
+    left = dp(x - 1, y)
+    if y != n: left *= p
+    down = dp(x, y - 1)
+    if x != m: down *= 1 - p
+    return left + down
+  print(float(dp(m, n - 1)))
+
+calc(10, 10, Fraction('1/101'))
+EOF
 
 echo Q3:
 for s in $(seq 0 3); do
